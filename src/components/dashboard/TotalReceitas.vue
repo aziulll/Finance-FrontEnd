d824ac04d7dd7f792c45b1d7c1b468266e57ccf4
Luiza <108764279+aziulll@users.noreply.github.com>
2023-11-24 09:44:21 -0300

    Update TotalReceitas

--- src/components/dashboard/TotalReceitas.vue
+++ src/components/dashboard/TotalReceitas.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import { useUserStore } from '../../stores/UserStore';
 
 export default {
@@ -16,14 +16,25 @@ export default {
       totalValue: 0,
     };
   },
-  async created() {
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/api/receita/total/13');
+  mounted() {
+    const userStore = useUserStore(); 
 
-      this.totalValue = response.data;
-      console.log(response);
+    // Se você tem o userId no seu store, use-o
+    const userId = userStore.userId;
+
+    if (!userId) {
+      console.error("UserId not found in useUserStore");
+      return;
+    }
+
+    try {
+      axios
+        .get(`http://127.0.0.1:8000/api/receita/total/${userId}`)
+        .then((response) => {
+          this.totalValue = response.data;
+        });
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching user:", error);
     }
   },
 };
